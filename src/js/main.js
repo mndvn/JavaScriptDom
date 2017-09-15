8fa299edb48ad20003c9a01c1088c844565015a9
mndvn <omer.mindivanli@gmail.com>
2017-09-15 16:20:08 +0300

    Bugs Fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,19 @@
-// Java Script can be used to read, write and etc... to DOM!
-/* console.dir(document);
-console.log(document.html);
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.body); */
+    function Changer(){
+        var newColor = document.getElementById("name");
+        newColor.style.color = 'red' ;
+    };
 
-function Changer(){
-    var newColor = document.getElementById("name");
-    newColor.style.color = 'red' ;
-}
+    function ReChanger(){
+        var exColor = document.getElementById("name");
+        exColor.style.color = 'black';
+    };
 
-function ReChanger(){
-    var exColor = document.getElementById("name");
-    exColor.style.color = 'black';
-}
+    function addDiv(){
+        var newDiv = document.getElementById("div");
+        newDiv.appendChild();
+    };
+
+
+
+
 
